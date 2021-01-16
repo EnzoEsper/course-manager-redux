90519270a28668e8663d4775be756b004c8f875f
enzoesper <enzoesper@hotmail.com>
2021-01-16 17:12:45 -0300

    converting the class-based component to a function-based for using hooks

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,37 +1,29 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from 'prop-types';
 
-class ManageCoursePage extends Component {
-
-  componentDidMount() {
-
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
-
+const ManageCoursePage = ({ courses, authors, loadAuthors, loadCourses }) => {
+  useEffect(() => {
     if (courses.length === 0) {
-      loadCourses()
-        .catch(error => {
-          alert("Loading courses failed ", error);
-        })
+      loadCourses().catch((error) => {
+        alert("Loading courses failed ", error);
+      });
     }
 
     if (authors.length === 0) {
-      loadAuthors()
-        .catch(error => {
-          alert("Loading authors failed ", error);
-        })
+      loadAuthors().catch((error) => {
+        alert("Loading authors failed ", error);
+      });
     }
-  }
+  }, []);
 
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
