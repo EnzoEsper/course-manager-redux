28c19ab73404a4b838d68a2349b16744a2753f9e
enzoesper <enzoesper@hotmail.com>
2021-01-16 02:45:08 -0300

    connecting the container component CoursesPage with the Redux store

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,33 +1,48 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as courseActions from "../../redux/actions/courseActions";
+import PropTypes from 'prop-types';
 class CoursesPage extends Component {
-
   state = {
     course: {
-      title: ""
-    }
-  }
+      title: "",
+    },
+  };
 
   handleChange = (event) => {
     const course = { ...this.state.course, title: event.target.value };
     this.setState({ course });
-  }
+  };
 
   handleSubmit = (event) => {
     event.preventDefault();
-    alert(this.state.course.title);
-  }
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  };
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h3>Add Course</h3>
-        <input type="text" onChange={this.handleChange} value={this.state.course.title} />
-        <input type="submit" value="Save"/>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="Save" />
       </form>
     );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  dispatch: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => {
+  return {
+    courses: state.courses,
+  };
+};
+
+export default connect(mapStateToProps)(CoursesPage);
